1f8832de1f4825e28bd2d9b665d065875362480a
Alper Nebi Yasak <alpernebiyasak@gmail.com>
2016-10-27 20:45:09 +0300

    Restructure test tar files

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -9,13 +9,13 @@ from albumin.utils import make_tar
 
 class TestUtils(TestCase):
     @with_temp_folder
-    @from_tar('repo-empty.tar.gz')
+    @from_tar('repo-tars/empty.tar.gz')
     def test_make_tar_file(self, cwd):
         with tempfile.NamedTemporaryFile(suffix='.tar.gz') as tar_file:
             make_tar(tar_file, cwd)
 
     @with_temp_folder
-    @from_tar('repo-empty.tar.gz')
+    @from_tar('repo-tars/empty.tar.gz')
     def test_make_tar_path(self, cwd):
         with tempfile.NamedTemporaryFile(suffix='.tar.gz') as tar_file:
             tmp_name = tar_file.name
