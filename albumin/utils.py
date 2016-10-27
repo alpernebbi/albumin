542131eb5b704112fead3cf68f5f08187f93de84
Alper Nebi Yasak <alpernebiyasak@gmail.com>
2016-10-27 12:26:55 +0300

    Make make_tar support file objects

--- albumin/utils.py
+++ albumin/utils.py
@@ -1,9 +1,17 @@
 import os
 import tarfile
+import functools
 
 
-def make_tar(tar_path, dir_path):
+@functools.singledispatch
+def make_tar(tar_file, dir_path):
+    with tarfile.open(fileobj=tar_file, mode='w:gz') as tar:
+        tar.add(dir_path, arcname='')
+
+
+@make_tar.register(str)
+def _(tar_file, dir_path):
     if not os.path.exists(dir_path) or not os.path.isdir(dir_path):
         raise ValueError("Folder {} doesn't exist.".format(dir_path))
-    with tarfile.open(tar_path, mode='w:gz') as tar:
+    with tarfile.open(tar_file, mode='w:gz') as tar:
         tar.add(dir_path, arcname='')
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -9,6 +9,11 @@ from albumin.utils import make_tar
 class TestUtils(TestCase):
     @with_temp_folder
     @from_tar('repo-empty.tar.gz')
-    def test_make_tar(self, cwd):
+    def test_make_tar_file(self, cwd):
         with tempfile.TemporaryFile() as tar_file:
             make_tar(tar_file, cwd)
+
+    @with_temp_folder
+    @from_tar('repo-empty.tar.gz')
+    def test_make_tar_path(self, cwd):
+        make_tar("/tmp/temp_tar.tar.gz", cwd)
