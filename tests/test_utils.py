429a753996c2337291a63ce4c38055cc2536af25
Alper Nebi Yasak <alpernebiyasak@gmail.com>
2016-10-28 10:39:12 +0300

    Refactor with_folder in test utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,5 @@
 from unittest import TestCase
-from tests.utils import with_temp_folder
-from tests.utils import from_tar
+from tests.utils import with_folder
 import tempfile
 import os
 
@@ -8,14 +7,12 @@ from albumin.utils import make_tar
 
 
 class TestUtils(TestCase):
-    @with_temp_folder
-    @from_tar('repo-tars/empty.tar.gz')
+    @with_folder('repo-tars/empty.tar.gz')
     def test_make_tar_file(self, temp_folder):
         with tempfile.NamedTemporaryFile(suffix='.tar.gz') as tar_file:
             make_tar(tar_file, temp_folder)
 
-    @with_temp_folder
-    @from_tar('repo-tars/empty.tar.gz')
+    @with_folder('repo-tars/empty.tar.gz')
     def test_make_tar_path(self, temp_folder):
         with tempfile.NamedTemporaryFile(suffix='.tar.gz') as tar_file:
             tmp_name = tar_file.name
