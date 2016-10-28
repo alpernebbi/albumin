6ddc909ee3fe4632039be1a9817a91f896fbfff9
Alper Nebi Yasak <alpernebiyasak@gmail.com>
2016-10-28 22:33:10 +0300

    Add argument parser

--- albumin/albumin.py
+++ albumin/albumin.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python3
 
+import argparse
+
+
+def argument_parser():
+    parser = argparse.ArgumentParser(
+        description="Manage photographs using a git-annex repository.")
+
+    parser.add_argument(
+        'repo_path',
+        help="path of the git-annex repository",
+        metavar='repo-path')
+
+    return parser
+
 
 def main():
-    pass
+    parser = argument_parser()
+    args = parser.parse_args()
 
 
 if __name__ == "__main__":
