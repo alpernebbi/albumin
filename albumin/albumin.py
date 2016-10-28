9ffe910ac2d706283078cf9c8553662c981c0aa2
Alper Nebi Yasak <alpernebiyasak@gmail.com>
2016-10-28 22:49:00 +0300

    Add --import argument

--- albumin/albumin.py
+++ albumin/albumin.py
@@ -12,12 +12,25 @@ def argument_parser():
         help="path of the git-annex repository",
         metavar='repo-path')
 
+    parser.add_argument(
+        '--import',
+        dest="import_path",
+        help="import pictures from the given path",
+        metavar="path")
+
     return parser
 
 
 def main():
     parser = argument_parser()
     args = parser.parse_args()
+
+    if args.import_path:
+        import_(**vars(args))
+
+
+def import_(repo_path, import_path, **kwargs):
+    raise NotImplementedError
 
 
 if __name__ == "__main__":
