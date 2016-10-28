bceb99f95a9a1ca44eb874dd854b060b368e9a1a
Alper Nebi Yasak <alpernebiyasak@gmail.com>
2016-10-28 09:53:57 +0300

    Rename cwd parameter to temp_folder in tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -10,14 +10,14 @@ from albumin.utils import make_tar
 class TestUtils(TestCase):
     @with_temp_folder
     @from_tar('repo-tars/empty.tar.gz')
-    def test_make_tar_file(self, cwd):
+    def test_make_tar_file(self, temp_folder):
         with tempfile.NamedTemporaryFile(suffix='.tar.gz') as tar_file:
-            make_tar(tar_file, cwd)
+            make_tar(tar_file, temp_folder)
 
     @with_temp_folder
     @from_tar('repo-tars/empty.tar.gz')
-    def test_make_tar_path(self, cwd):
+    def test_make_tar_path(self, temp_folder):
         with tempfile.NamedTemporaryFile(suffix='.tar.gz') as tar_file:
             tmp_name = tar_file.name
-        make_tar(tmp_name, cwd)
+        make_tar(tmp_name, temp_folder)
         os.remove(tmp_name)
