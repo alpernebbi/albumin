7a6624e8fd11d9e5296269b6a42d145de5895557
Alper Nebi Yasak <alpernebiyasak@gmail.com>
2016-10-29 20:00:47 +0300

    Add bash script to strip image data

---
+++ tests/images/strip-image.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+name=$(basename "$1")
+ext="${name##*.}"
+name="${name%.*}"
+new="${name}.meta.${ext}"
+
+convert "$1" -resize 1x1 "$new"
+exiftool -overwrite_original_in_place -ThumbnailImage= -PreviewImage= "$new"
+touch "$new" --reference="$1"
+
+diff <(exiftool "$1") <(exiftool "$new") -U 3
