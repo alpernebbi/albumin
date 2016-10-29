158971ae571df4560722fae5a30ff1f8fa56326c
Alper Nebi Yasak <alpernebiyasak@gmail.com>
2016-10-29 20:20:41 +0300

    Implement exiftool tag retrieval

--- albumin/imdate.py
+++ albumin/imdate.py
@@ -1,4 +1,18 @@
+from exiftool import ExifTool
 
 
 def analyze_date(file_path):
     raise NotImplementedError
+
+
+def exiftool_generator(tags):
+    with ExifTool() as exif_tool:
+        file_path = yield None
+        while file_path:
+            tags_ = exif_tool.get_tags(tags, file_path)
+            file_path = yield tags_
+
+
+exiftool_tags = [
+    'EXIF:DateTimeOriginal',
+    'EXIF:CreateDate']
