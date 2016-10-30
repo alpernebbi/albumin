bd0d976c524d83c540ccb5511ee64fd2bcd2f382
Alper Nebi Yasak <alpernebiyasak@gmail.com>
2016-10-30 17:43:35 +0300

    Add two-way mapping utility

--- albumin/utils.py
+++ albumin/utils.py
@@ -2,6 +2,7 @@ import os
 import tarfile
 import functools
 import re
+import collections.abc
 
 
 def files_in(dir_path, relative=False):
@@ -30,6 +31,60 @@ def sequenced_folder_name(parent_path):
         next_alpha, next_int = last_alpha, last_int + 1
 
     return '{}{:03}'.format(next_alpha, next_int)
+
+
+class MutualMapping(collections.abc.MutableMapping):
+    def __init__(self):
+        self.map_a = {}
+        self.map_b = {}
+
+    def invert(self):
+        self.map_a, self.map_b = self.map_b, self.map_a
+        return self
+
+    def __getitem__(self, key):
+        if key in self.map_a:
+            return self.map_a[key]
+        elif key in self.map_b:
+            return self.map_b[key]
+        else:
+            raise KeyError(key)
+
+    def __contains__(self, key):
+        return key in self.map_a or key in self.map_b
+
+    def __setitem__(self, key_a, key_b):
+        for key in [key_a, key_b]:
+            if key in self.map_a:
+                self.map_b.pop(self.map_a.pop(key))
+            if key in self.map_b:
+                self.map_a.pop(self.map_b.pop(key))
+        self.map_a[key_a] = key_b
+        self.map_b[key_b] = key_a
+
+    def __delitem__(self, key):
+        if key in self.map_a:
+            key_b = self.map_a[key]
+            del self.map_b[key_b]
+            del self.map_a[key]
+        elif key in self.map_b:
+            key_a = self.map_b[key]
+            del self.map_a[key_a]
+            del self.map_b[key]
+        else:
+            raise KeyError(key)
+
+    def __iter__(self):
+        yield from self.map_a
+
+    def __len__(self):
+        return len(self.map_a)
+
+    def __repr__(self):
+        items = '{}={{}}, ' * len(self.map_a)
+        items = items.format(*self.map_a.keys())
+        items = items.format(*self.map_a.values())
+        return 'MutualMapping({})'.format(items[:-2])
 
 
 @functools.singledispatch
