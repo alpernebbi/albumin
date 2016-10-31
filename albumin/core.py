a6034263be7aa40c7930a683b5065275fe28444a
Alper Nebi Yasak <alpernebiyasak@gmail.com>
2016-10-31 20:20:21 +0300

    Add metadata handling to --import

--- albumin/core.py
+++ albumin/core.py
@@ -13,12 +13,47 @@ def import_(repo_path, import_path, **kwargs):
     repo = GitAnnexRepo(repo_path)
     current_branch = repo.branches[0]
 
+    import_files = list(files_in(import_path))
+    import_file_data, err = analyze_date(*import_files, chainmap=False)
+    import_keys = {f: repo.annex.calckey(f) for f in import_files}
+    if err:
+        raise err
+
+    import_data = {}
+    for method, map_ in import_file_data.items():
+        import_data[method] = {}
+        for file, data in map_.items():
+            key = import_keys[file]
+            if import_data[method].get(key, data) != data:
+                err_msg = ('Multiple results for same file: {} '
+                           'has {} and {}.')
+                err_msg = err_msg.format(key, import_data[key], data)
+                raise RuntimeError(err_msg)
+            import_data[method][key] = data
+
     repo.checkout('albumin-imports')
     repo.annex.import_(import_path)
     import_name = os.path.basename(import_path)
     batch_name = sequenced_folder_name(repo_path)
     repo.move(import_name, batch_name)
     repo.commit("Import batch {} ({})".format(batch_name, import_name))
+
+    common_keys = repo.annex.keys & set(import_keys.values())
+    repo_data = repo_datetimes(repo, common_keys, chainmap=False)
+
+    chain = []
+    for method in method_order:
+        chain.append(import_data.get(method, {}))
+        chain.append(repo_data.get(method, {}))
+    final_data = ChainMap(*chain)
+
+    repo_chain = ChainMap(*[repo_data[m] for m in method_order])
+
+    for key, data in final_data.items():
+        if repo_chain.get(key, None) != data:
+            dt_string = data.datetime.strftime('%Y-%m-%d@%H-%M-%S')
+            repo.annex[key]['datetime'] = dt_string
+            repo.annex[key]['datetime-method'] = data.method
 
     if current_branch:
         repo.checkout(current_branch)
