9396775b16c2fb99db65b0e3ac80b1bd068e856f
Alper Nebi Yasak <alpernebiyasak@gmail.com>
2016-11-01 14:19:28 +0300

    Refactor metadata operations
    Replace the chainmaps with the new ImageDate class which can be
    sorted (and which used to be a namedtuple), base the metadata
    operations on that. Additionally, refactor metadata updates to
    standalone functions, and make --analyze use them.

--- albumin/core.py
+++ albumin/core.py
@@ -1,6 +1,5 @@
 import os
 from datetime import datetime
-from collections import ChainMap
 
 from albumin.gitrepo import GitAnnexRepo
 from albumin.utils import sequenced_folder_name
@@ -13,23 +12,9 @@ def import_(repo_path, import_path, **kwargs):
     repo = GitAnnexRepo(repo_path)
     current_branch = repo.branches[0]
 
-    import_files = list(files_in(import_path))
-    import_file_data, err = analyze_date(*import_files, chainmap=False)
-    import_keys = {f: repo.annex.calckey(f) for f in import_files}
-    if err:
-        raise err
-
-    import_data = {}
-    for method, map_ in import_file_data.items():
-        import_data[method] = {}
-        for file, data in map_.items():
-            key = import_keys[file]
-            if import_data[method].get(key, data) != data:
-                err_msg = ('Multiple results for same file: {} '
-                           'has {} and {}.')
-                err_msg = err_msg.format(key, import_data[key], data)
-                raise RuntimeError(err_msg)
-            import_data[method][key] = data
+    updates, remaining = get_datetime_updates(repo, import_path)
+    if remaining:
+        raise NotImplementedError(remaining)
 
     repo.checkout('albumin-imports')
     repo.annex.import_(import_path)
@@ -38,22 +23,7 @@ def import_(repo_path, import_path, **kwargs):
     repo.move(import_name, batch_name)
     repo.commit("Import batch {} ({})".format(batch_name, import_name))
 
-    common_keys = repo.annex.keys & set(import_keys.values())
-    repo_data = repo_datetimes(repo, common_keys, chainmap=False)
-
-    chain = []
-    for method in method_order:
-        chain.append(import_data.get(method, {}))
-        chain.append(repo_data.get(method, {}))
-    final_data = ChainMap(*chain)
-
-    repo_chain = ChainMap(*[repo_data[m] for m in method_order])
-
-    for key, data in final_data.items():
-        if repo_chain.get(key, None) != data:
-            dt_string = data.datetime.strftime('%Y-%m-%d@%H-%M-%S')
-            repo.annex[key]['datetime'] = dt_string
-            repo.annex[key]['datetime-method'] = data.method
+    apply_datetime_updates(repo, updates)
 
     if current_branch:
         repo.checkout(current_branch)
@@ -61,105 +31,106 @@ def import_(repo_path, import_path, **kwargs):
 
 def analyze(analyze_path, repo_path=None, **kwargs):
     analyze_files = list(files_in(analyze_path))
-    analyze_data, remaining = analyze_date(*analyze_files)
-    only_repo, same, different = {}, {}, {}
+    overwrites, additions, keys = {}, {}, {}
 
     if repo_path:
         print('Compared to repo: {}'.format(repo_path))
         repo = GitAnnexRepo(repo_path)
-        analyze_keys = {f: repo.annex.calckey(f) for f in analyze_files}
-        common_keys = repo.annex.keys & set(analyze_keys.values())
-        repo_data = repo_datetimes(repo, common_keys)
+        keys = {f: repo.annex.calckey(f) for f in analyze_files}
+        updates, remaining = get_datetime_updates(repo, analyze_path)
 
-        if repo_data:
-            for file in remaining:
-                key = analyze_keys[file]
-                if key in repo_data:
-                    only_repo[file] = repo_data[key]
-                    del remaining[file]
+        for file, key in keys.items():
+            if key in updates:
+                datum, old_datum = updates[key]
+                if old_datum:
+                    overwrites[file] = (datum, old_datum, key)
+                else:
+                    additions[file] = datum
 
-            analyze_data = analyze_data.new_child()
-            for file, value in analyze_data.items():
-                key = analyze_keys[file]
-                if key in repo_data:
-                    if repo_data[key] == value:
-                        same[file] = repo_data[key]
-                        analyze_data[file] = None
-                    else:
-                        different[file] = (repo_data[key], value)
-                        analyze_data[file] = None
+        rem_keys = {k for f, k in keys.items() if f in remaining}
+        rem_data = get_repo_datetimes(repo, rem_keys)
+        for file in remaining:
+            if rem_data.get(keys[file], None):
+                remaining.pop(file)
 
-            if same:
-                print()
-                print("Some files have metadata matching the repo:")
-                for file in sorted(same):
-                    print('    {}: {} ({})'.format(
-                        file,
-                        same[file].datetime,
-                        same[file].method))
+    else:
+        additions, remaining = analyze_date(*analyze_files)
 
-            if only_repo:
-                print()
-                print('Some files have metadata only in the repo:')
-                for file in sorted(only_repo):
-                    print('    {}: {} ({})'.format(
-                        file,
-                        only_repo[file].datetime,
-                        only_repo[file].method))
+    modified = set.union(*map(set, (overwrites, additions, remaining)))
+    redundants = set(analyze_files) - modified
+    if redundants:
+        print("No new information: ")
+        for file in sorted(redundants):
+            print('    {}'.format(file))
 
-            if different:
-                print()
-                print('Some files have contradictory info:')
-                for file in sorted(different):
-                    print('    {}: {} ({}) vs {} ({}) (repo)'.format(
-                        file,
-                        different[file][1].datetime,
-                        different[file][1].method,
-                        different[file][0].datetime,
-                        different[file][0].method))
+    if additions:
+        print("New files: ")
+        for file in sorted(additions):
+            datum = additions[file]
+            print('    {}: {}'.format(file, datum))
 
-    if set(analyze_data.values()) != {None}:
-        print()
-        print('New files:')
-        for file in sorted(analyze_data):
-            if (file in same) or (file in different):
-                continue
-            print('    {}: {} ({})'.format(
-                file,
-                analyze_data[file].datetime,
-                analyze_data[file].method
-            ))
+    if overwrites:
+        print("New information: ")
+        for file in sorted(overwrites):
+            (datum, old_datum, key) = overwrites[file]
+            print('    {}: {} => {}'.format(file, old_datum, datum))
+            print('        (from {})'.format(key))
 
     if remaining:
-        print()
-        print("Some files have no datetime information:")
-        for file in remaining:
+        print("No information: ")
+        for file in sorted(remaining):
             print('    {}'.format(file))
 
 
-def repo_datetimes(repo, keys, chainmap=True):
-    maps = {method: {} for method in method_order}
+def get_datetime_updates(repo, update_path):
+    files = list(files_in(update_path))
+    file_data, remaining = analyze_date(*files)
+    keys = {f: repo.annex.calckey(f) for f in files}
+
+    def conflict_error(key, data_1, data_2):
+        err_msg = ('Conflicting results for file: \n'
+                   '    {}:\n    {} vs {}.')
+        return RuntimeError(err_msg.format(key, data_1, data_2))
+
+    data = {}
+    for file, datum in file_data.items():
+        key = keys[file]
+        if key in data and data[key] == datum:
+            if data[key].datetime != datum.datetime:
+                raise conflict_error(key, data[key], data)
+        data[key] = max(data.get(key), datum)
+
+    common_keys = repo.annex.keys & set(data)
+    repo_data = get_repo_datetimes(repo, common_keys)
+
+    updates = {}
+    for key, datum in data.items():
+        if datum > repo_data.get(key):
+            updates[key] = (datum, repo_data.get(key))
+
+    return updates, remaining
+
+
+def apply_datetime_updates(repo, updates):
+    for key, (datum, _) in updates.items():
+        dt_string = datum.datetime.strftime('%Y-%m-%d@%H-%M-%S')
+        repo.annex[key]['datetime'] = dt_string
+        repo.annex[key]['datetime-method'] = datum.method
+
+
+def get_repo_datetimes(repo, keys):
+    data = {}
     for key in keys:
         dt_string = repo.annex[key]['datetime']
         method = repo.annex[key]['datetime-method']
 
         try:
             dt = datetime.strptime(dt_string, '%Y-%m-%d@%H-%M-%S')
-            data = ImageDate(method, dt)
-            maps[method][key] = data
+            datum = ImageDate(method, dt)
+            data[key] = datum
         except ValueError:
-            continue
+            data[key] = None
         except TypeError:
-            continue
+            data[key] = None
 
-    if chainmap:
-        ordered_maps = [maps[method] for method in method_order]
-        return ChainMap(*ordered_maps)
-    else:
-        return maps
-
-
-method_order = [
-    'Manual',
-    'DateTimeOriginal',
-    'CreateDate']
+    return data
--- albumin/imdate.py
+++ albumin/imdate.py
@@ -1,49 +1,75 @@
 from exiftool import ExifTool
 from datetime import datetime
-from collections import namedtuple
-from collections import ChainMap
 
 
-def analyze_date(*file_paths, chainmap=True):
-    results = from_exif(*file_paths, chainmap=chainmap)
-    if chainmap:
-        done = results.keys()
-    else:
-        done_parts = (set(map_.keys()) for map_ in results.values())
-        done = set.union(*done_parts)
-    remaining = set(file_paths) - done
-
-    if remaining:
-        return results, remaining
-    return results, None
+def analyze_date(*file_paths):
+    results = from_exif(*file_paths)
+    remaining = {f for f in file_paths if f not in results}
+    return results, remaining
 
 
-ImageDate = namedtuple('ImageDate', ['method', 'datetime'])
+class ImageDate:
+    methods = [
+        'ManualTrusted',
+        'DateTimeOriginal',
+        'CreateDate',
+        'ManualUntrusted'
+    ]
+
+    def __init__(self, method, datetime):
+        self.method = method.split(':')[-1]
+        if self.method not in ImageDate.methods:
+            raise ValueError(method)
+        self.datetime = datetime
+
+    @property
+    def order(self):
+        return ImageDate.methods.index(self.method)
+
+    def __lt__(self, other):
+        return self.order > other.order if other else False
+
+    def __gt__(self, other):
+        return self.order < other.order if other else True
+
+    def __eq__(self, other):
+        return self.order == other.order if other else False
+
+    def __ne__(self, other):
+        return self.order != other.order if other else True
+
+    def __le__(self, other):
+        return self.order >= other.order if other else False
+
+    def __ge__(self, other):
+        return self.order <= other.order if other else True
+
+    def __repr__(self):
+        repr_ = "ImageDate(method={!r}, datetime={!r})"
+        return repr_.format(self.method, self.datetime)
+
+    def __str__(self):
+        return '{} ({})'.format(self.datetime, self.method)
 
 
-def from_exif(*file_paths, chainmap=True):
+def from_exif(*file_paths):
+    if not file_paths:
+        return {}
+
     with ExifTool() as tool:
         tags_list = tool.get_tags_batch(exiftool_tags, file_paths)
-    method = lambda tag : tag.split(':')[-1]
 
-    maps = {method(tag): {} for tag in exiftool_tags}
+    data = {}
     for tags in tags_list:
-        for tag in tags:
-            file = tags['SourceFile']
-            datetime_ = tags[tag]
-
+        file = tags['SourceFile']
+        for tag, dt_string in tags.items():
             try:
-                dt = datetime.strptime(datetime_, '%Y:%m:%d %H:%M:%S')
-                data = ImageDate(tag.split(':')[-1], dt)
-                maps[method(tag)][file] = data
+                dt = datetime.strptime(dt_string, '%Y:%m:%d %H:%M:%S')
+                datum = ImageDate(tag, dt)
+                data[file] = max(data.get(file), datum)
             except ValueError:
                 continue
-
-    if chainmap:
-        ordered_maps = [maps[method(tag)] for tag in exiftool_tags]
-        return ChainMap(*ordered_maps)
-    else:
-        return maps
+    return data
 
 
 def exiftool_generator(tags):
