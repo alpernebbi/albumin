fcdbdd71f18ccd981973077073f2a45fdde318dc
Alper Nebi Yasak <alpernebiyasak@gmail.com>
2016-12-28 20:25:31 +0300

    Add subsecond datetime formats

--- albumin/imdate.py
+++ albumin/imdate.py
@@ -88,7 +88,9 @@ class ImageDate:
 
     datetime_formats = [
         '%Y:%m:%d %H:%M:%S',
+        '%Y:%m:%d %H:%M:%S.%f',
         '%Y-%m-%d@%H-%M-%S'
+        '%Y-%m-%d@%H-%M-%S.%f'
     ]
 
     def __init__(self, method, datetime_):
